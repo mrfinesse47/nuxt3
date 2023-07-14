9874627c95c3dc4f588453b068e9a1080b7e262b
Kevin Mason <krmason2020@gmail.com>
2023-07-14 07:48:03 -0700

    ,

---
+++ nuxt-dojo/pages/products/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>/products</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis amet
+      voluptas eius voluptate maxime perspiciatis, adipisci delectus porro,
+      quisquam dignissimos nulla maiores, consectetur itaque eaque modi est
+      tenetur nisi? Veniam eveniet mollitia aperiam similique inventore illum.
+      Exercitationem vero explicabo tenetur ipsum nostrum ducimus, quas sunt
+      rerum dignissimos, mollitia aliquam porro.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat eaque et
+      atque sequi quibusdam nisi, suscipit nemo ab commodi. Recusandae
+      voluptatibus delectus quas nulla quo asperiores enim fugit, voluptatem
+      maiores aut molestias cumque doloremque perferendis aperiam veritatis,
+      eaque fugiat fuga nemo tempore eius? Ad magnam officia recusandae quam
+      beatae similique.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
